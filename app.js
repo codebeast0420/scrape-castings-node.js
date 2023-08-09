56dac60b0f21d077961725af11681140f8c356f2
John <codebeast0420@gmail.com>
2023-08-09 09:01:14 -0400

    add link for maxi casting

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.post("/get-castings", async (req, res) => {
 				casting.name = $(el).find("h2 a").text().replace(/[\n\t]+/g, ' ').trim();
 				casting.date = $(el).find("p span").text().replace(/[\n\t]+/g, ' ').trim();
 				casting.description = $(el).find("div").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.link = $(el).find("h2 a").attr('href');
 			}
 			if (req.body.castingId == 7) {
 				casting.name = $(el).find("h3").text().replace(/[\n\t]+/g, ' ').trim();
