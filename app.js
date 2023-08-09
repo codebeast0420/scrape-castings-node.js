fab7daab49bf4ed519b59b7ce90fa17461bdbf8c
John <codebeast0420@gmail.com>
2023-08-09 04:27:24 -0400

    complete api for castprod

--- app.js
+++ app.js
@@ -2,33 +2,86 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const pretty = require("pretty");
 const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const url = "https://www.maxicasting.com/castings";
+const app = express();
+app.use(bodyParser.json());
 
-async function scrapeData() {
+app.use(cors({
+	origin: '*'
+}));
+
+app.post("/get-castings", async (req, res) => {
+	console.log('req', req.body);
+	let url = '';
+	switch (req.body.castingId) {
+		case 0: url = "https://www.maxicasting.com/castings"; break;
+		case 1: url = "https://www.allcasting.fr/castings/ "; break;
+		case 2: url = "https://www.casting.fr/castings"; break;
+		case 3: url = "https://www.123casting.com/castings"; break;
+		case 4: url = "https://www.etoilecasting.com/#/"; break;
+		case 5: url = "https://figurants.com/casting/region-parisienne-r782097"; break;
+		case 6: url = "https://bookme.fr/"; break;
+		default: url = 'https://castprod.com/';
+	}
+
+
 	try {
 		// Fetch HTML of the page we want to scrape
 		const { data } = await axios.get(url);
 		const $ = cheerio.load(data);
-		const listItems = $(".blogPage article .liste_details");
+		let listItems = '';
+		switch (req.body.castingId) {
+			case 0: listItems = $(".blogPage article .liste_details"); break;
+			case 1: listItems = $(".blogPage article .liste_details"); break;
+			case 2: listItems = $(".blogPage article .liste_details"); break;
+			case 3: listItems = $(".blogPage article .liste_details"); break;
+			case 4: listItems = $(".blogPage article .liste_details"); break;
+			case 5: listItems = $(".blogPage article .liste_details"); break;
+			case 6: listItems = $(".blogPage article .liste_details"); break;
+			default: listItems = $(".job_list .job_cards .job-card");
+		}
+
 		const castings = [];
-		listItems.each((idx, el) => {
-			const casting = { name: "", date: "" };
-			casting.name = $(el).find("h2 a").text().replace(/[\n\t]+/g, ' ').trim();
-			casting.date = $(el).find("p span").text().replace(/[\n\t]+/g, ' ').trim();
+		listItems.each(async (idx, el) => {
+			const casting = { name: "", date: "", place: "", description: "", link: "", category: "" };
+			if (req.body.castingId == 0) {
+				casting.name = $(el).find("h2 a").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.date = $(el).find("p span").text().replace(/[\n\t]+/g, ' ').trim();
+			}
+			if (req.body.castingId == 7) {
+				casting.name = $(el).find("h3").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.place = $(el).find("p span").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.description = $(el).find("p").clone().find("span").remove().end().text().replace(/[\n\t]+/g, ' ').trim().replace(/\\|"/g, '');
+				casting.link = "https://castprod.com/poste/" + casting.name.replace(/\s+/g, '-').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9-]/g, "");
+			}
 			castings.push(casting);
 		});
 		console.dir(castings);
-		fs.writeFile("maxcasting.json", JSON.stringify(castings, null, 2), (err) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			console.log("Successfully written data to file");
-		});
+		let fileName = '';
+		switch (req.body.castingId) {
+			case 0: fileName = "maxicasting.json"; break;
+			case 1: fileName = "maxicasting.json"; break;
+			case 2: fileName = "maxicasting.json"; break;
+			case 3: fileName = "maxicasting.json"; break;
+			case 4: fileName = "maxicasting.json"; break;
+			case 5: fileName = "maxicasting.json"; break;
+			case 6: fileName = "maxicasting.json"; break;
+			default: fileName = "castProd.json";
+		}
+		// fs.writeFile(fileName, JSON.stringify(castings, null, 2), (err) => {
+		// 	if (err) {
+		// 		console.error(err);
+		// 		return;
+		// 	}
+		// 	console.log("Successfully written data to file");
+		// });
+		res.send(castings);
 	} catch (err) {
 		console.error(err);
 	}
-}
-// Invoke the above function
-scrapeData();
+})
+
+app.listen(5000, () => console.log("Servier is listening to port 5000"));
