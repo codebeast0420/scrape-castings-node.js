c3b5e2fb2a1b4b9873ae38b40a6e0e65ee3d2cb2
John <codebeast0420@gmail.com>
2023-08-10 22:49:28 -0400

    add etoile casting scrape

--- app.js
+++ app.js
@@ -25,15 +25,24 @@ app.post("/get-castings-page", async (req, res) => {
 		case 1: url = "https://www.allcasting.fr/castings/"; break;
 		case 2: url = "https://www.casting.fr/castings"; break;
 		case 3: url = "https://www.123casting.com/castings"; break;
-		case 4: url = "https://www.etoilecasting.com/#/"; break;
+		case 4: url = "https://www.etoilecasting.com/"; break;
 		case 5: url = "https://figurants.com/casting/region-parisienne-r782097"; break;
 		case 6: url = "https://bookme.fr/"; break;
 		default: url = 'https://castprod.com/';
 	}
 
+	let pageLink = '';
+	if (req.body.castingId == 2) {
+		pageLink = "?cl_page=" + req.body.page;
+	}
+
+	if (req.body.castingId == 4) {
+		pageLink = "?page=" + req.body.page + "#/";
+	}
+
 	try {
 		// Fetch HTML of the page we want to scrape
-		const { data } = await axios.get(url + "?cl_page=" + req.body.page);
+		const { data } = await axios.get(url + pageLink);
 		const $ = cheerio.load(data);
 		let listItems = '';
 		switch (req.body.castingId) {
@@ -41,7 +50,7 @@ app.post("/get-castings-page", async (req, res) => {
 			case 1: listItems = $(".entry-content-data"); break;
 			case 2: listItems = $(".casting-list-content div div div div .casting-box-content div .col-box-default"); break;
 			case 3: listItems = $(".blogPage article .liste_details"); break;
-			case 4: listItems = $(".blogPage article .liste_details"); break;
+			case 4: listItems = $(".castings .row div div .card-body"); break;
 			case 5: listItems = $(".blogPage article .liste_details"); break;
 			case 6: listItems = $(".blogPage article .liste_details"); break;
 			default: listItems = $(".job_list .job_cards .job-card");
@@ -58,27 +67,15 @@ app.post("/get-castings-page", async (req, res) => {
 				casting.link = $(el).find(".casting-box-default a").attr('href');
 			}
 
+			if (req.body.castingId == 4) {
+				casting.name = $(el).find("a h2").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.date = $(el).find("a p:nth-child(2)").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.description = $(el).find("a p").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.category = $(el).find("a div .casting-tag").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.link = $(el).find("a").attr('href');
+			}
 			castings.push(casting);
 		});
-		// console.dir(castings);
-		let fileName = '';
-		switch (req.body.castingId) {
-			case 0: fileName = "maxicasting.json"; break;
-			case 1: fileName = "allcasting.json"; break;
-			case 2: fileName = "casting.json"; break;
-			case 3: fileName = "123casting.json"; break;
-			case 4: fileName = "etoilecasting.json"; break;
-			case 5: fileName = "figurants.json"; break;
-			case 6: fileName = "bookme.json"; break;
-			default: fileName = "castProd.json";
-		}
-		// fs.writeFile(fileName, JSON.stringify(castings, null, 2), (err) => {
-		// 	if (err) {
-		// 		console.error(err);
-		// 		return;
-		// 	}
-		// 	console.log("Successfully written data to file");
-		// });
 		res.send(castings);
 
 	} catch (err) {
@@ -109,8 +106,8 @@ app.post("/get-castings", async (req, res) => {
 			case 0: listItems = $(".blogPage article .liste_details"); break;
 			case 1: listItems = $(".entry-content-data"); break;
 			case 2: listItems = $(".casting-list-content div div div div .casting-box-content div .col-box-default"); break;
-			case 3: listItems = $(".blogPage article .liste_details"); break;
-			case 4: listItems = $(".blogPage article .liste_details"); break;
+			case 3: listItems = $("#castings .content .all_castings #result_castings div"); break;
+			case 4: listItems = $(".castings .row div div .card-body"); break;
 			case 5: listItems = $(".blogPage article .liste_details"); break;
 			case 6: listItems = $(".blogPage article .liste_details"); break;
 			default: listItems = $(".job_list .job_cards .job-card");
@@ -165,10 +162,19 @@ app.post("/get-castings", async (req, res) => {
 			}
 
 			if (req.body.castingId == 3) {
-				casting.name = $(el).find("h2 a").text().replace(/[\n\t]+/g, ' ').trim();
-				casting.date = $(el).find("p span").text().replace(/[\n\t]+/g, ' ').trim();
-				casting.description = $(el).find("div").text().replace(/[\n\t]+/g, ' ').trim();
-				casting.link = $(el).find("h2 a").attr('href');
+				casting.name = $(el).find(".description h3 a").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.date = $(el).find(".infos").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.description = $(el).find(".description p").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.link = $(el).find(".description h3 a").attr('href');
+			}
+
+			if (req.body.castingId == 4) {
+				casting.name = $(el).find("a h2").text().replace(/[\n\t]+/g, ' ').trim();
+				const detail = $(el).find("a p").text().trim().split('\n');
+				casting.date = detail[1].replace(/[\n\t]+/g, ' ').trim()
+				casting.description = detail[0].replace(/[\n\t]+/g, ' ').trim();
+				casting.category = $(el).find("a div .casting-tag").text().replace(/[\n\t]+/g, ' ').trim();
+				casting.link = $(el).find("a").attr('href');
 			}
 
 			if (req.body.castingId == 7) {
@@ -191,13 +197,13 @@ app.post("/get-castings", async (req, res) => {
 			case 6: fileName = "bookme.json"; break;
 			default: fileName = "castProd.json";
 		}
-		// fs.writeFile(fileName, JSON.stringify(castings, null, 2), (err) => {
-		// 	if (err) {
-		// 		console.error(err);
-		// 		return;
-		// 	}
-		// 	console.log("Successfully written data to file");
-		// });
+		fs.writeFile(fileName, JSON.stringify(castings, null, 2), (err) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log("Successfully written data to file");
+		});
 		res.send(castings);
 
 	} catch (err) {
